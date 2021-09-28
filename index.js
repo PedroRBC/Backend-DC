a855f94df174f9fa4128db1a82e59563b2d9b936
Mih ai <bestrplcean12@gmail.com>
2021-09-28 21:50:52 +0300

    My first commit

--- index.js
+++ index.js
@@ -22,13 +22,10 @@ app.use(session({
 }))
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'https://happy-saha-d5b576.netlify.app');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(cors({
+	origin: ["http://localhost:3000"],
+	credentials: true
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/api', aplicatie)
