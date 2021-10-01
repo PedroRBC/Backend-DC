3ca35c65738bb54bb43af0f70e85c6a521118672
He1utu <56837883+He1utu@users.noreply.github.com>
2021-10-01 10:56:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     token: "", // token from bot
     prefix: "v-", // default prefix
     database: "", // link from mongodb
-    clientId: "", //client id from disocrd
-    clientSecret: "", // client secret from discord
+    clientId: "", //client id from bot
+    clientSecret: "", // client secret from bot
     callbackurl: "/api/auth/discord/redirect", // callback
     port: 1337, // port
     FRONDEND_URL: "http://localhost:3000" // Front-End URL
